0c49a614d6308977bfffc45c1365f678e47d55c6
Matias <matiaszurita933@gmail.com>
2022-09-26 14:30:12 -0300

    Cree la primera vista

--- mazu_proyecto1/mazu_app1/views.py
+++ mazu_proyecto1/mazu_app1/views.py
@@ -1,3 +1,8 @@
+from urllib import request
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def vistaUno(request):
+    s = "<h1>Hola a la Evaluacion 1</h1><br><hr><p>hablar hablar hablar</p><button>Inicio</button>"
+    return HttpResponse(s)
